b0e5110efa420021605428fcfdf53d076bd89bc6
ImCakir <caglaccakir@gmail.com>
2025-07-09 16:58:10 +0300

    08. ödev: employee tablosu oluşturuldu ve sorular çözüldü

---
+++ 08-homework/01-create-employee-table.sql
@@ -0,0 +1,10 @@
+-- Soru 1: test veritabanınızda employee isimli sütun bilgileri id(INTEGER),
+ name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+
+-- 1. employee tablosunu oluşturma
+CREATE TABLE employee (
+    id INTEGER,
+    name VARCHAR(50),
+    birthday DATE,
+    email VARCHAR(100)
+);
