814939aff4ca477bd1c5bb1565d445157a1d0ebc
ImCakir <caglaccakir@gmail.com>
2025-07-11 19:16:00 +0300

    12. ödev 2. soru: en yüksek rental_rate değerine sahip film sayısı sorgusu eklendi

---
+++ 12-homework/02-count-films-with-max-rental_rate.sql
@@ -0,0 +1,9 @@
+-- SORU 2: film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+
+SELECT COUNT(*) 
+FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+
+
+
