3a7c9f8d33d9f27a7e8a194871b337a66e472d7a
ImCakir <caglaccakir@gmail.com>
2025-07-11 18:05:30 +0300

    10. Ödev: LEFT JOIN sorgusu eklendi

---
+++ 10-homework/01-left-join-city-country.sql
@@ -0,0 +1,6 @@
+-- Sour 1: city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country)
+--isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+
+SELECT country.country, city.city 
+FROM city
+LEFT JOIN country ON country.country_id = city.country_id;
